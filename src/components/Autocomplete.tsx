d4c306bbc39578412520b3657840bb4207a426ce
Aleksander Chaika <alekschayka97@gmail.com>
2026-04-10 17:58:29 +0300

    Solution

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { Person } from '../types/Person';
+
+type Props = {
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+};
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  onSelected,
+  delay = 300,
+}) => {
+  const [query, setQuery] = useState('');
+  const [debouncedQuery, setDebouncedQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [query, delay]);
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(person =>
+      person.name.toLowerCase().includes(debouncedQuery.toLowerCase()),
+    );
+  }, [people, debouncedQuery]);
+
+  return (
+    <>
+      <div className={isOpen ? 'dropdown is-active' : 'dropdown'}>
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onFocus={() => setIsOpen(true)}
+            onChange={event => {
+              setQuery(event.target.value);
+              onSelected(null);
+              setIsOpen(true);
+            }}
+          />
+        </div>
+
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className="dropdown-content">
+            {filteredPeople.map(person => (
+              <div
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                key={person.slug}
+                onMouseDown={() => {
+                  setQuery(person.name);
+                  onSelected(person);
+                  setIsOpen(false);
+                }}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {isOpen && filteredPeople.length === 0 && (
+        <div
+          className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
